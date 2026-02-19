858df4068b22a9d4c2a2f571bbf48e67cf7cdcec
Riff <8172612+xuyang1220@users.noreply.github.com>
2026-02-19 23:27:43 +0800

    Initial DeepFM on Criteo (PyTorch)

---
+++ src/utils/.ipynb_checkpoints/seed-checkpoint.py
@@ -0,0 +1,14 @@
+import os
+import random
+import numpy as np
+import torch
+
+
+def seed_everything(seed: int):
+    random.seed(seed)
+    np.random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = False
+    torch.backends.cudnn.benchmark = True
